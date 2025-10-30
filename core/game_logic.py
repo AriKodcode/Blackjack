a78bdf8e4b3a9a929ea49c2a8db69813a3085c2a
Ari Durlacher <arikodcode@gmail.com>
2025-10-30 11:55:57 +0200

    fixed fonction run_full_game

--- core/game_logic.py
+++ core/game_logic.py
@@ -45,8 +45,6 @@ def dealer_play(deck: list[dict], dealer: dict) -> bool:
 
 def run_full_game(deck: list[dict], player: dict, dealer: dict) -> None:
     print("wellcome to black jack game\ndood luck!")
-    print("Press Enter to start")
-    input("")
     status_game = ""
     deal_two_each(deck,player,dealer)
     game = True

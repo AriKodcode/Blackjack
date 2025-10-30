3e77468a099c3b81ad2b8e3269e68ad13c485b4d
Ari Durlacher <arikodcode@gmail.com>
2025-10-30 10:30:04 +0200

    create fonction deal_tow_each and fixed fonction calculate_hand_value

--- core/game_logic.py
+++ core/game_logic.py
@@ -1,11 +1,29 @@
+from core.deck import build_standard_deck,shuffle_by_suit
+
 def calculate_hand_value(hand: list[dict]) -> int:
     sum_value_cards = 0
     for card in hand:
-        sum_value_cards += card["rank"]
+        if card["rank"] == "J":
+            sum_value_cards += 10
+        elif card["rank"] == "Q":
+            sum_value_cards += 10
+        elif card["rank"] == "K":
+            sum_value_cards += 10
+        elif card["rank"] == "A":
+            sum_value_cards += 1
+        else:
+            sum_value_cards += int(card["rank"])
     return sum_value_cards
 
+
 def deal_two_each(deck: list[dict], player: dict, dealer: dict) -> None:
-    return None
+    player["hand"].append(deck.pop())
+    player["hand"].append(deck.pop())
+    dealer["hand"].append(deck.pop())
+    dealer["hand"].append(deck.pop())
+    print("player value cards: ", calculate_hand_value(player["hand"]))
+    print("dealer value cards: ", calculate_hand_value(dealer["hand"]))
+
 
 
 def dealer_play(deck: list[dict], dealer: dict) -> bool:

f697df421b56781faabba261eb02e00241aba8c5
Ari Durlacher <arikodcode@gmail.com>
2025-10-30 10:52:33 +0200

    create fonction ask_player_action

--- core/player_io.py
+++ core/player_io.py
@@ -1,4 +1,17 @@
 def ask_player_action() -> str:
-    return ""
+    while True:
+        user_input = input("Press S or H")
+        if not user_input.isdigit():
+            if user_input == "S":
+                break
+            elif user_input == "H":
+                break
+            else:
+                print("Error press S or H only")
+                continue
+        else:
+            print("Error prees H or S only")
+    return user_input
 
 
+print(ask_player_action())
